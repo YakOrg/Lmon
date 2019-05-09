2ff0b99d36efa1eebe04d754ddd9311db95ae4dd
Sokket <sokket@oceancraft.ru>
2019-05-09 23:46:13 +0300

    CPU load avg, RAM total and usage

--- main.c
+++ main.c
@@ -1,16 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <err.h>
+#include <fcntl.h>
+#include <sys/sysinfo.h>
 
-char response[] = "HTTP/1.1 200 OK\r\n"
-                  "Content-Type: application/json; charset=UTF-8\r\n\r\n{"
-                  "}";
+double getCPULoadAvg() {
+    int FileHandler;
+    char FileBuffer[1024];
+    float load;
+
+    FileHandler = open("/proc/loadavg", O_RDONLY);
+    if (FileHandler < 0) {
+        return -1;
+    }
+    read(FileHandler, FileBuffer, sizeof(FileBuffer) - 1);
+    sscanf(FileBuffer, "%f", &load);
+    close(FileHandler);
+    return (double) load;
+}
+
+int getMemAttr(char *attr) {
+    char *base = ": %d kB";
+    char *pattern = malloc(strlen(attr) + strlen(base));
+    sprintf(pattern, "%s%s", attr, base);
+
+    FILE *stream = fopen("/proc/meminfo", "r");
+    if (stream == NULL) return -1;
+
+    char *line = malloc(512);
+    while (fgets(line, 50, stream)) {
+        int attrValue;
+        if (sscanf(line, pattern, &attrValue)) {
+            fclose(stream);
+            free(line);
+            free(pattern);
+            return attrValue;
+        }
+    }
+    free(line);
+    free(pattern);
+    fclose(stream);
+    return -1;
+}
 
 int main() {
     int one = 1, client_fd;
@@ -18,9 +57,7 @@ int main() {
     socklen_t sin_len = sizeof(cli_addr);
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-        err(1, "can't open socket");
-
+    if (sock < 0) err(1, "can't open socket");
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 
     int port = 8080;
@@ -34,16 +71,34 @@ int main() {
     }
 
     listen(sock, 5);
-    while (1) {
-        client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
-        printf("got connection\n");
 
-        if (client_fd == -1) {
-            perror("Can't accept");
+    for (;;) {
+        client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+
+        char buf[512];
+        if (recv(client_fd, buf, 512, 0) < 0) {
+            // user disconnected or timeout (if you set a timeout)
+            // NO call to exit; use "continue" or "return", or something else
+            // to gracefully handle the break;
             continue;
         }
 
-        write(client_fd, response, sizeof(response) - 1); /*-1:'\0'*/
+        char *response = malloc(1024);
+        sprintf(response,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: application/json; charset=UTF-8\r\n\r\n"
+                "{\"load_avg\":%f, "
+                "\"ram_size\":%d, "
+                "\"ram_usage\":%d"
+                "}",
+                getCPULoadAvg(),
+                getMemAttr("MemTotal") / 1000,
+                getMemAttr("MemTotal") / 1000 - getMemAttr("MemAvailable") / 1000);
+        int i = 0;
+        for (; i < strlen(response) && i != '}'; i++) {}
+        char *resp = malloc(i + 1);
+        memcpy(resp, response, i);
+        write(client_fd, resp, i);
         close(client_fd);
     }
 }
