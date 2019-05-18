3fd0084be8bcb209fd87ea0d67833e11878932d1
Garin <ylicezar@yandex.ru>
2019-05-18 11:30:54 +0000

    added daemon function

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
-void start_agent_as_daemon(int port){
-    /*TODO*/
-    /*there is should be a daemon code*/
+void write_daemon_log(){/*TODO*/}
+
+void daemon_start_agent(int port){
+    if(daemon(0,0) != 0){
+        /*fail*/
+        fprintf(STDERR_FILENO, "can`t start agent as daemon");
+        exit(EXIT_FAILURE);
+    }
+
     startAgent(port);
 };
 
@@ -11,7 +17,7 @@ void printUsage() {
             "NAME:\n   lmon - monitoring, but small and simple\n\n"
             "USAGE:\n   lmon [commands] [arguments...]\n\n"
             "COMMANDS:\n   server   Run management server\n   agent    Run node agent\n\n"
-            "ARGS:\n   --http-port <PORT> http server port\n   --help             show this help\n   --daemon         start agent as daemon"
+            "ARGS:\n   --http-port <PORT> http server port\n   --help             show this help\n   --daemon           start agent as daemon"
     );
 }
 
@@ -23,19 +29,23 @@ int main(int argc, char **argv) {
 
     int httpPort = 8080;
     char *enptr;
+    int as_daemon = 0;
 
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "--http-port") == 0 && i + 1 < argc)
             httpPort = strtol(argv[i + 1], &enptr, 10);
-        if(strcmp(argv[i], "--daemon") == 0)
-           start_agent_as_daemon(httpPort);
+        if (strcmp(argv[i], "--daemon") == 0)
+            as_daemon = 1;
     }
 
     if (strcmp(argv[1], "server") == 0)
         startServer(httpPort);
-    else if (strcmp(argv[1], "agent") == 0)
-        startAgent(httpPort);
-    else printUsage();
+    else if (strcmp(argv[1], "agent") == 0){
+        if(as_daemon == 1)
+            daemon_start_agent(httpPort);
+        else
+            startAgent(httpPort);
+    }else printUsage();
 
     return 0;
 }
