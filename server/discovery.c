d5a017dde90c658850c34c13150efceb5d3e65dd
Garin <ylicezar@yandex.ru>
2019-05-24 12:57:52 +0000

    Update 'server/discovery.c'

--- server/discovery.c
+++ server/discovery.c
@@ -3,17 +3,6 @@
 //
 
 #include "discovery.h"
-#include "../agent/metrics/metrics.h"
-#include "../http/http.h"
-
-#include <stdio.h>      /* for printf() and fprintf() */
-#include <sys/socket.h> /* for socket() and bind() */
-#include <arpa/inet.h>  /* for sockaddr_in */
-#include <stdlib.h>     /* for atoi() and exit() */
-#include <string.h>     /* for memset() */
-#include <unistd.h>     /* for close() */
-#include <err.h>
-#include <pthread.h>
 
 pthread_t tid;
 agent *agents;
