0d1571cfc0f550d41e0c974026ac1b342181ca79
Garin <ylicezar@yandex.ru>
2019-05-24 12:58:10 +0000

    Update 'server/discovery.h'

--- server/discovery.h
+++ server/discovery.h
@@ -5,6 +5,17 @@
 #ifndef LMON_DISCOVERY_H
 #define LMON_DISCOVERY_H
 
+#include "../agent/metrics/metrics.h"
+#include "../http/http.h"
+
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket() and bind() */
+#include <arpa/inet.h>  /* for sockaddr_in */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+#include <err.h>
+#include <pthread.h>
 #include <netinet/in.h>
 
 void startServer(int httpPort);
