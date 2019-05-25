078308b579c2a1dfd177d47abfe1144f5d8cdfae
Sokket <sokket@oceancraft.ru>
2019-05-25 09:17:44 +0300

    libcurl integration

--- agent/agent.h
+++ agent/agent.h
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netdb.h>
+#include <curl/curl.h>
 #include <microhttpd.h>
 
 #include "../http/http.h"
@@ -15,5 +17,9 @@
 #include "json/json.h"
 
 void start_agent(int http_port, char *server_url);
+
+char *start_broadcast_listener(char *port);
+
+void *get_in_addr(struct sockaddr *sa);
 
 #endif //LMON_AGENT_H
